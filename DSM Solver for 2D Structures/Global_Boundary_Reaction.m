1d4a083f0c0eefe63e8f590e447d681fde2b50b0
Chengyou Yue <112973740+TensorYue@users.noreply.github.com>
2025-08-21 16:47:19 -0400

    Delete DSM Solver for 2D Structures directory

--- DSM Solver for 2D Structures/Global_Boundary_Reaction.m
+++
@@ -1,3 +0,0 @@
-function F_R = Global_Boundary_Reaction(K_global, U_global, F_E, F_Q, Boundary_Restriction_List) 
-    F_R = (K_global * U_global + F_E - F_Q) .* Boundary_Restriction_List; % F_H has been included in U_Global when processing
-end
--- DSM Solver for 2D Structures/Stability_Check.m
+++
@@ -1,9 +0,0 @@
-function Stability = Stability_Check(K_structural_special)
-    dim = size(K_structural_special, 1);
-    rk = rank(K_structural_special);
-    if dim == rk
-        Stability = 'Structure is stable';
-    else
-        Stability = 'Structure is unstable';
-    end
-end
